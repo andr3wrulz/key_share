056b09e127af6f12fbb23d08446abe3937eea058
Andrew Geltz <geltz.andrew@gmail.com>
2019-08-03 13:01:13 -0400

    Initial key_share model setup

---
+++ key_share_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KeyShareAppConfig(AppConfig):
+    name = 'key_share_app'
---
+++ key_share_app/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Key(models.Model):
+    # Data fields
+    name = models.CharField(max_length=255)
+    key = models.CharField(max_length=100)
+    notes = models.CharField(max_length=500, null=True, blank=True)
+    created = models.DateTimeField('created timestamp', auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField('last updated timestamp', auto_now=True, editable=False)
+    redeemed = models.DateTimeField('redeemed timestamp', null=True, blank=True)
+
+    # Relationship fields
+    parent_key = models.ForeignKey(
+        'Key', # Another instance of this object
+        related_name='child_key',
+        null=True, # Store blank as NULL
+        blank=True, # Not required
+        on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        User, # Django User model
+        related_name='submitted_keys',
+        on_delete=models.CASCADE
+    )
+    redeemed_by = models.ForeignKey(
+        User, # Django User model
+        related_name='redeemed_keys',
+        null=True, # Store blank as NULL
+        blank=True, # Not required
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
