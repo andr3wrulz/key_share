56d3205f0fbc991fa99d8e8418c50457bf0d440a
Andrew Geltz <geltz.andrew@gmail.com>
2019-08-04 23:31:03 -0400

    More data for the detail page

--- key_share_app/views.py
+++ key_share_app/views.py
@@ -1,6 +1,7 @@
 from enum import Enum
 import requests
 import re
+import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
@@ -39,79 +40,123 @@ def key_detail(request, pk):
     # Try to retrieve key from the database
     key = get_object_or_404(Key, pk=pk) # Get requested key from db
 
+    game_info = {}
+
     # ===== Initial Game query =====
     game = requests.get(
         'https://api-v3.igdb.com/games/',
         headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
-        data='search "' + key.name + '"; fields name,total_rating,total_rating_count,summary,url,time_to_beat,cover,category,genres,websites,first_release_date,involved_companies,keywords,multiplayer_modes,similar_games,storyline,videos; limit 1;'
+        data='search "' + key.name + '"; fields name,total_rating,total_rating_count,summary,url,time_to_beat,cover,category,genres,websites,first_release_date,involved_companies,keywords,multiplayer_modes,similar_games,storyline,screenshots,videos; limit 1;'
     ).json()
-    #print(game)
+    print(game)
 
-    game_info = {}
-    game_info["name"] = game[0].get("name")
-    game_info["summary"] = game[0].get("summary")
-    game_info["storyline"] = game[0].get("storyline")
-    game_info["igdbLink"] = game[0].get("url")
-    game_info["rating"] = game[0].get("total_rating")
-    game_info["release_date"] = game_info.get("first_release_date")
-    game_info["rating_count"] = game[0].get("total_rating_count")
-    game_info["type"] = game_categories[game[0].get("category")]
+    if len(game) == 1:
+        # ===== Directly available data
+        game_info["name"] = game[0].get("name")
+        game_info["summary"] = game[0].get("summary")
+        game_info["storyline"] = game[0].get("storyline")
+        game_info["igdbLink"] = game[0].get("url")
+        game_info["rating"] = game[0].get("total_rating")
+        game_info["rating_count"] = game[0].get("total_rating_count")
+        game_info["release_date"] = datetime.datetime.fromtimestamp(game[0].get("first_release_date"))
+        game_info["type"] = game_categories[game[0].get("category")]
 
-    # ===== Genre lookup =====
-    genreIdString = ', '.join(map(str, game[0].get("genres"))).strip('[]') # Join ids together like '7, 8, 31, 32'
-    genres = requests.get(
-        'https://api-v3.igdb.com/genres',
-        headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
-        data='where id = ({}); fields name;'.format(genreIdString)
-    ).json()
-    #print(genres)
+        # ===== Cover lookup =====
+        cover = requests.get(
+            'https://api-v3.igdb.com/covers',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = {}; fields url,height,width; limit 1;'.format(game[0].get("cover"))
+        ).json()
+        #print(cover)
 
-    # Extract genre names from list of dicts like:
-    # [{'id': 31, 'name': 'Adventure'}, {'id': 7, 'name': 'Music'}, {'id': 32, 'name': 'Indie'}, {'id': 8, 'name': 'Platform'}]
-    game_info["genres"] = [genre.get("name") for genre in genres]
+        if len(cover) == 1:
+            game_info["cover"] = cover[0]
+            # Grab the full size cover instead of the thumbnail they try to give us
+            # //images.igdb.com/igdb/image/upload/t_thumb/qx1e1miroreqtlv6ndt2.jpg -> //images.igdb.com/igdb/image/upload/t_cover_big/qx1e1miroreqtlv6ndt2.jpg
+            game_info["cover"]["url"] = re.sub(r't_thumb','t_cover_big',game_info["cover"].get("url"))
 
-    # ===== Cover lookup =====
-    cover = requests.get(
-        'https://api-v3.igdb.com/covers',
-        headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
-        data='where id = {}; fields url,height,width; limit 1;'.format(game[0].get("cover"))
-    ).json()
-    #print(cover)
+        # ===== Genre lookup =====
+        genre_id_string = ', '.join(map(str, game[0].get("genres"))).strip('[]') # Join ids together like '7, 8, 31, 32'
+        genres = requests.get(
+            'https://api-v3.igdb.com/genres',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = ({}); fields name;'.format(genre_id_string)
+        ).json()
+        #print(genres)
 
-    game_info["cover"] = cover[0]
-    # Grab the full size cover instead of the thumbnail they try to give us
-    # //images.igdb.com/igdb/image/upload/t_thumb/qx1e1miroreqtlv6ndt2.jpg -> //images.igdb.com/igdb/image/upload/t_cover_big/qx1e1miroreqtlv6ndt2.jpg
-    game_info["cover"]["url"] = re.sub(r't_thumb','t_cover_big',game_info["cover"].get("url"))
+        # Extract genre names from list of dicts like:
+        # [{'id': 31, 'name': 'Adventure'}, {'id': 7, 'name': 'Music'}, {'id': 32, 'name': 'Indie'}, {'id': 8, 'name': 'Platform'}]
+        game_info["genres"] = [genre.get("name") for genre in genres]
 
-    # ===== Website lookup =====
-    websiteIdString = ', '.join(map(str, game[0].get("websites"))).strip('[]') # Join ids together like '7, 8, 31, 32'
-    websites = requests.get(
-        'https://api-v3.igdb.com/websites',
-        headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
-        data='where id = ({}); fields category,url;'.format(websiteIdString)
-    ).json()
-    #print(websites)
+        # ===== Involved company lookup =====
+        involved_companies_string = ', '.join(map(str, game[0].get("involved_companies"))).strip('[]') # Join ids together like '7, 8, 31, 32'
+        involved_companies = requests.get(
+            'https://api-v3.igdb.com/involved_companies',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = ({}) & developer = true; fields company; limit 1;'.format(involved_companies_string)
+        ).json()
+        if len(involved_companies) == 1:
+            developer = requests.get(
+                'https://api-v3.igdb.com/companies',
+                headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+                data='where id = {}; fields name; limit 1;'.format(involved_companies[0].get("id"))
+            ).json()
+            if len(developer) == 1:
+                game_info["developer"] = developer[0].get("name")
 
-    # Rip website data from following format based on category:
-    # [{'id': 56823, 'category': 1, 'url': 'http://wanderso.ng'}, {'id': 56824, 'category': 13, 'url': 'https://store.steampowered.com/app/530320'}]
-    game_info["websites"] = {}
-    for w in websites:
-        game_info["websites"][ websiteTypes[w.get("category") - 1] ] = w.get("url")
+        # ===== Keyword lookup =====
+        keyword_string = ', '.join(map(str, game[0].get("keywords"))).strip('[]') # Join ids together like '7, 8, 31, 32'
+        keywords = requests.get(
+            'https://api-v3.igdb.com/keywords',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = ({}); fields name;'.format(keyword_string)
+        ).json()
 
-    # ===== Time to beat lookup =====
+        # Extract keywords from list of dicts like:
+        # [{"id": 386,"name": "platformers"},{"id": 117,"name": "indie"}, {"id": 4702,"name": "bards"},{"id": 1687,"name": "music based"}]
+        game_info["keywords"] = [kw.get("name") for kw in keywords]
 
-    # ===== Involved company lookup =====
+        # ===== Multiplayer mode lookup =====
+        # TODO
 
-    # ===== Keyword lookup =====
+        # ===== Screenshot lookup =====
+        screenshot_string = ', '.join(map(str, game[0].get("screenshots"))).strip('[]') # Join ids together like '7, 8, 31, 32'
+        screenshot = requests.get(
+            'https://api-v3.igdb.com/screenshots',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = ({}); fields url;'.format(screenshot_string)
+        ).json()
 
-    # ===== Multiplayer mode lookup =====
+        # Extract image urls from list of dicts like:
+        # [ {"id": 123973,"url": "//images.igdb.com/igdb/image/upload/t_thumb/qt39h1bbio87yijaylyq.jpg"},{"id": 123974,"url": "//images.igdb.com/igdb/image/upload/t_thumb/f1jlivj9zipslgmrapik.jpg"}]
+        # Grab the full size cover instead of the thumbnail they try to give us
+        # //images.igdb.com/igdb/image/upload/t_thumb/qx1e1miroreqtlv6ndt2.jpg -> //images.igdb.com/igdb/image/upload/t_1080p/qx1e1miroreqtlv6ndt2.jpg
+        game_info["screenshots"] = [re.sub(r't_thumb','t_1080p',ss.get("url")) for ss in screenshot]
 
-    # ===== Screenshot lookup =====
+        # ===== Similar game lookup =====
 
-    # ===== Similar game lookup =====
+        # ===== Time to beat lookup =====
 
-    # ===== Video lookup =====
+        # ===== Video lookup =====
+        
+        # ===== Website lookup =====
+        website_id_string = ', '.join(map(str, game[0].get("websites"))).strip('[]') # Join ids together like '7, 8, 31, 32'
+        websites = requests.get(
+            'https://api-v3.igdb.com/websites',
+            headers={'user-key':'2ba120e8696301c4263b2b19e68e59e6'},
+            data='where id = ({}); fields category,url;'.format(website_id_string)
+        ).json()
+        #print(websites)
 
+        # Rip website data from following format based on category:
+        # [{'id': 56823, 'category': 1, 'url': 'http://wanderso.ng'}, {'id': 56824, 'category': 13, 'url': 'https://store.steampowered.com/app/530320'}]
+        game_info["websites"] = {}
+        for w in websites:
+            game_info["websites"][ websiteTypes[w.get("category") - 1] ] = w.get("url")
+    
+    # End if len(game) == 1
+
+    # ===== Return response =====
     print(game_info)
 
     # If we found the key, pass it to the detail page
